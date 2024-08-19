2a41adcd64b1de31a2b3d7c9f74d05386e459532
AtharvaLotankar11 <164212107+AtharvaLotankar11@users.noreply.github.com>
2024-08-19 12:11:48 +0530

    Delete script3.js

--- script3.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("toggleInfoBtn").addEventListener("click", function() {
-    const memberInfo = document.querySelectorAll('.member-info');
-    memberInfo.forEach(info => {
-        info.style.display = info.style.display === 'none' ? 'block' : 'none';
-    });
-});
